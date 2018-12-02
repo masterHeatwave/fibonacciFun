7559dc7e7035867718e180f2a340bcdf1d4c9346
masterHeatwave <38331772+masterHeatwave@users.noreply.github.com>
2018-12-03 00:16:33 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace fibonacciSeq
+{
+    class fProgram
+    {
+		static int p1 = 0, p2 = 1, p3;
+
+        static void Main(string[] args)
+
+        {
+			Console.WriteLine("Please provide the initial value:");
+
+			int count = Int32.Parse(Console.ReadLine());
+
+			Console.Write(p1 + " " + p2);
+
+			for (int i = 2; i < count; i++)
+
+			{
+				p3 = p1 + p2;
+
+				Console.Write(" " + p3);
+
+				p1 = p2;
+
+				p2 = p3;
+			}
+
+			Console.WriteLine();
+
+		}
+          
+	}
+
+}
